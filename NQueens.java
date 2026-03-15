4ed6d2ed99529aff331fe5e5cc9f2ede60686299
Caleb C <kaichun24@gmail.com>
2026-03-15 14:03:11 -0700

    steepest done

--- NQueens.java
+++ NQueens.java
@@ -1,21 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NQueens {
 
     public static void main(String[] args) {
-        int[] board = generateRandomBoard(8);
-        steepestHillClimbing(board, evalQueens(board));
+        runSteepestMultiple(10000, 16);
     }
 
     // Steepest Hill-Climbing Alg
-    private static int[][] steepestHillClimbing(int[] board, int numCorrect) {
+    private static int[] steepestHillClimbing(int[] board) {
         int[] curr = board;
-        boolean isDone = false;
-        while (!isDone) {
-            int[] mostAmazingNeighbor = null;
-            int mostExquisiteNeighborEvaluation = evalQueens(curr);
-            
+        int[] bestNeighbor = curr;
+        int bestNeighborEval = evalQueens(curr);
+        while (67 == 67) {
+            int[][] allNeighbors = produceNeighbors(bestNeighbor); // holds an array of all neighbors      
+            List<int[]> highestNeighbors = new ArrayList<>(); // holds a list of best heuristic neighbors
 
+            int localBestNeighborEval = bestNeighborEval;
+            for (int[] neighbor : allNeighbors) {
+                int currEval = evalQueens(neighbor);
+
+                if (currEval < localBestNeighborEval) {
+                    localBestNeighborEval = currEval;
+                    highestNeighbors.clear();
+                    highestNeighbors.add(neighbor);
+                } else if (currEval == localBestNeighborEval && currEval < bestNeighborEval) {
+                    highestNeighbors.add(neighbor);
+                }
+            }
+
+            // if highest neighbors heuristic is the same, then return
+            if (highestNeighbors.isEmpty()) return null;
+
+            // Pick random neighbor
+            int randIdx = (int) (Math.random() * (highestNeighbors.size()));
+            if (!highestNeighbors.isEmpty()) {
+                bestNeighbor = highestNeighbors.get(randIdx);
+                bestNeighborEval = evalQueens(bestNeighbor);
+            }
+
+            // if new best eval == number of queens
+            if (bestNeighborEval == 0) return bestNeighbor;
         }
-        return null;
     }
 
     // Evaluate Correct Queens
@@ -32,7 +58,42 @@ public class NQueens {
                 }
             }
         }
-        return board.length - numIncorrect;
+        return numIncorrect;
+    }
+
+    // Get all Neighbors of current state
+    private static int[][] produceNeighbors(int[] board) {
+        int n = board.length;
+        int[][] neighbors = new int[n * (n - 1)][n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i] != j) {
+                    int copy[] = board.clone();
+                    copy[i] = j;
+                    neighbors[count++] = copy;
+                }
+            }
+        } 
+        return neighbors;
+    }
+
+    private static void runSteepestMultiple(int iterations, int queens) {
+        int successes = 0;
+        for (int i = 0; i < iterations; i++) {
+            int[] board = generateRandomBoard(queens);
+            int[] solution = steepestHillClimbing(board);
+            if (solution == null) {
+                System.out.println("Steepest Hill Climb has failed!");
+            } else {
+                System.out.println("Solution:");
+                // printCurrentBoard(solution);
+                successes++;
+            }
+        }
+
+        double sr = ((double) successes / (double) iterations) * 100;
+        System.out.print("Board Size: " + queens + "\nTotal Iterations: " + iterations + "\nSuccesses: " + successes + "\nSuccess Rate: " + String.format("%.2f", sr) + "%");
     }
 
     // Generation Functions
